af6027a65f7ee26c5baefe76ae2ceaeeedd016c8
Nostea <kiwiuchi@googlemail.com>
2025-03-12 18:54:05 +0100

    Initial commit

---
+++ src/main/java/de/home/playgrounds/javabasics/tutorial2/VowelCounter.java
@@ -0,0 +1,7 @@
+package de.home.playgrounds.javabasics.tutorial2;
+
+public class VowelCounter {
+    public static void main(String[] args) {
+        System.out.println("TODO: Count all vowels from program arguments");
+    }
+}

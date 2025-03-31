944e54243a8b4f8e5c04f590a67b387acc08a45b
Nostea <kiwiuchi@googlemail.com>
2025-03-31 22:08:10 +0200

    added 2 pieces of gold, 5 turns wait time for snake to move

--- src/main/java/de/home/playgrounds/javabasics/example1_ObjectsStates/Znake.java
+++ src/main/java/de/home/playgrounds/javabasics/example1_ObjectsStates/Znake.java
@@ -2,17 +2,20 @@ package de.home.playgrounds.javabasics.example1_ObjectsStates;
 
 public class Znake {
 
-    public static void main( String[] args ) {
 
+
+    public static void main( String[] args ) {
+        int playerMoveCounter = 0;
         java.awt.Point playerPosition = new java.awt.Point( 10, 9 );
         java.awt.Point snakePosition  = new java.awt.Point( 30, 2 );
         java.awt.Point goldPosition   = new java.awt.Point( 6, 6 );
+        java.awt.Point goldPosition2   = new java.awt.Point( 4, 4 );
         java.awt.Point doorPosition   = new java.awt.Point( 0, 5 );
         boolean rich = false;
 
         while ( true ) {
             // Raster mit Figuren zeichnen
-
+            //40x10 grid zeichnen
             for ( int y = 0; y < 10; y++ ) {
                 for ( int x = 0; x < 40; x++ ) {
                     java.awt.Point p = new java.awt.Point( x, y );
@@ -20,7 +23,7 @@ public class Znake {
                         System.out.print( '&' );
                     else if ( snakePosition.equals( p ) )
                         System.out.print( 'S' );
-                    else if ( goldPosition.equals( p ) )
+                    else if ( goldPosition.equals( p ) || goldPosition2.equals(p) )
                         System.out.print( '$' );
                     else if ( doorPosition.equals( p ) )
                         System.out.print( '#' );
@@ -40,30 +43,49 @@ public class Znake {
                 return;
             }
 
-            if ( playerPosition.equals( goldPosition ) ) {
+            if ( playerPosition.equals( goldPosition )  ) {
                 rich = true;
                 goldPosition.setLocation( -1, -1 );
             }
 
-            // Konsoleneingabe und Spielerposition verändern
-            switch ( new java.util.Scanner( System.in ).next() ) {
-                // Spielfeld ist im Bereich 0/0 .. 39/9
-                case "h" : playerPosition.y = Math.max(  0, playerPosition.y - 1 ); break;
-                case "t" : playerPosition.y = Math.min(  9, playerPosition.y + 1 ); break;
-                case "l" : playerPosition.x = Math.max(  0, playerPosition.x - 1 ); break;
-                case "r" : playerPosition.x = Math.min( 39, playerPosition.x + 1 ); break;
-
+            if ( playerPosition.equals(goldPosition2)) {
+                rich = true;
+                goldPosition2.setLocation(-1,-1);
             }
 
-            // Schlange bewegt sich in Richtung Spieler
-            if ( playerPosition.x < snakePosition.x )
-                snakePosition.x--;
-            else if ( playerPosition.x > snakePosition.x )
-                snakePosition.x++;
-            if ( playerPosition.y < snakePosition.y )
-                snakePosition.y--;
-            else if ( playerPosition.y > snakePosition.y )
-                snakePosition.y++;
+
+            // Konsoleneingabe und Spielerposition verändern
+            /*
+               Hätte man an dieser Stelle int playerMoveCounter = 0; initialisiert, dann würde wegen der WHILE Schleife in der sich der gesamte Code befindet,
+               die variable nach jedem Zug/Schleifeniteration auf 0 zurückgesetzt. Der Counter erfüllt die Bedingung >= 5 nie
+                playerMoveCounter muss außerhalb der WHILE schleife deklariert werden.
+             */
+            switch ( new java.util.Scanner(System.in).next() ) {
+                // Spielfeld ist im Bereich 0/0 .. 39/9
+                case "h" : playerPosition.y = Math.max(  0, playerPosition.y - 1 ); playerMoveCounter++; break;
+                case "t" : playerPosition.y = Math.min(  9, playerPosition.y + 1 ); playerMoveCounter++; break;
+                case "l" : playerPosition.x = Math.max(  0, playerPosition.x - 1 ); playerMoveCounter++; break;
+                case "r" : playerPosition.x = Math.min( 39, playerPosition.x + 1 ); playerMoveCounter++; break;
+                default:
+                    System.out.println("Ungültiger Zug! Verwende h, t, l, oder r.");
+                    break;
+            }
+
+            System.out.println(playerMoveCounter);
+
+
+            if (playerMoveCounter >= 5) {
+                // Schlange bewegt sich in Richtung Spieler
+                if ( playerPosition.x < snakePosition.x )
+                    snakePosition.x--;
+                else if ( playerPosition.x > snakePosition.x )
+                    snakePosition.x++;
+                if ( playerPosition.y < snakePosition.y )
+                    snakePosition.y--;
+                else if ( playerPosition.y > snakePosition.y )
+                    snakePosition.y++;
+            }
+
 
         } // end while
     }

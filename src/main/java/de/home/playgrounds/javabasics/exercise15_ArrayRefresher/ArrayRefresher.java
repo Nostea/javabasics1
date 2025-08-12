09561ad101660ecb041a5254d34c2e7a0643c8a0
Nostea <kiwiuchi@googlemail.com>
2025-08-12 20:43:39 +0200

    added more Array refresher exercises

--- src/main/java/de/home/playgrounds/javabasics/exercise15_ArrayRefresher/ArrayRefresher.java
+++ src/main/java/de/home/playgrounds/javabasics/exercise15_ArrayRefresher/ArrayRefresher.java
@@ -1,9 +1,48 @@
 package de.home.playgrounds.javabasics.exercise15_ArrayRefresher;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ArrayRefresher {
+
+    public static void findBiggestNum (int[] maxArray) {
+        int maxNum = maxArray[0];
+
+        for(int i = 1; i < maxArray.length; i++) {
+            if(maxNum < maxArray[i]) {
+                maxNum = maxArray[i];
+            }
+        }
+        System.out.println("MaxNum is: " + maxNum);
+
+    }
+
+    public static void findString(String[] stringArray, String searchWord) {
+
+        if(Arrays.asList(stringArray).contains(searchWord)){
+            System.out.println("TRUE");
+        } else {
+            System.out.println("FALSE");
+        }
+
+    }
+
+    public static List<Integer> findDuplicateInt(List<Integer> listOfNumbers){
+        Set<Integer> elements = new HashSet<Integer>();
+        return listOfNumbers.stream().filter(n -> !elements.add(n)).collect(Collectors.toList());
+    }
+
+
     public static void main(String[] args) {
+
+        System.out.println(findDuplicateInt(Arrays.asList(1,2,4,2,6,4,4,0,5)));
+
+        System.out.println("------");
+
+        findString(new String[]{"Apfel","Banane","Pfirsich"},"Apfel");
+
+        System.out.println("------");
+
         int [] beispielArray = { 1,2,3,4,5,6};
 
         for(int i= 0; i < beispielArray.length; i++) {
@@ -34,9 +73,24 @@ public class ArrayRefresher {
             }
         System.out.println(java.util.Arrays.toString(reverseArrayResult));
 
+        System.out.println("--------");
+
+        findBiggestNum(new int[]{1, 3, 5, 8});
+
+        System.out.println("--------");
+
+        double[] averageArray = {1.325, 5.2137, 39.98702, 27.07321};
+        double average = 0.0;
+
+        for(int i = 0; i < averageArray.length; i++) {
+            average += averageArray[i];
         }
+        average = average /  averageArray.length;
+        System.out.println("The average of " + java.util.Arrays.toString(averageArray) + " is: " + average);
 
+        System.out.println("--------");
 
+        }
 
     }
 

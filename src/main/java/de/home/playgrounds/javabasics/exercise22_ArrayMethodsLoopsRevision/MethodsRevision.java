ab5842b0380d7abe86015865dbf15a0aa2b80eb4
Nostea <kiwiuchi@googlemail.com>
2026-03-24 16:08:03 +0100

    added fizzbuzz

--- src/main/java/de/home/playgrounds/javabasics/exercise22_ArrayMethodsLoopsRevision/MethodsRevision.java
+++ src/main/java/de/home/playgrounds/javabasics/exercise22_ArrayMethodsLoopsRevision/MethodsRevision.java
@@ -11,8 +11,8 @@ public class MethodsRevision {
     }
 
     // Write method for maximum of 2 given numbers
-    public static void maximum (int num1, int num2) {
-        int maximum = Math.max(num1,num2);
+    public static void maximum(int num1, int num2) {
+        int maximum = Math.max(num1, num2);
         System.out.println("Maximum from given numbers " + num1 + ", " + num2 + " is: " + maximum);
     }
 
@@ -20,22 +20,44 @@ public class MethodsRevision {
     //Schreibe eine Methode euroZuDollar(double betrag), die einen Euro-Betrag in US-Dollar umrechnet (Kurs: 1 EUR = 1.08 USD).
     // Schreibe außerdem eine Methode dollarZuEuro(double betrag) für die Gegenrichtung.
     // Gib in main() beide Umrechnungen formatiert aus.
-    public static void euroToDollar (double euroValue) {
+    public static void euroToDollar(double euroValue) {
         double dollarValue = euroValue * 1.16;
         System.out.printf("%.2f EUR --> %.2f $ %n", euroValue, dollarValue);
     }
 
-    public static void dollarToEuro (double dollarValue) {
+    public static void dollarToEuro(double dollarValue) {
         double euroValue = dollarValue * 0.86;
         System.out.printf("%.2f $ --> %.2f EUR %n", dollarValue, euroValue);
     }
 
+    // Schreibe eine Methode fizzBuzz(int number), die alle Zahlen von 1 bis n durchläuft und folgendes ausgibt
+    // Zahl durch 3 teilbar --> Fizz
+    // Zahl durch 5 teilbar --> Buzz
+    // Ist die Zahl durch 3 und 5 teilbar --> FizzBuzz
+    // Sonst --> die Zahl selbst
+    public static void fizzBuzz(int number) {
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         squared(4);
-        maximum(23,3);
-        maximum(4,25);
+        maximum(23, 3);
+        maximum(4, 25);
         euroToDollar(100.44);
         dollarToEuro(100.44);
+        System.out.println();
+        fizzBuzz(15);
 
     }
 
